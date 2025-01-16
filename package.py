9df1fe45b5cfa9f46cd6989a3c23e3f0b9b29caf
Philippe Leprince <philippe@ynput.io>
2025-01-16 09:03:27 +0100

    Update package.py

--- package.py
+++ package.py
@@ -1,25 +1,27 @@
 # Required: lower case addon name e.g. 'deadline', otherwise addon
 #   will be invalid
-name = ""
+name = "substancedesigner"
 
 # Optional: Addon title shown in UI, 'name' is used by default e.g. 'Deadline'
-title = ""
+title = "Substance Designer"
 
 # Required: Valid semantic version (https://semver.org/)
 version = "0.0.0"
 
 # Name of client code directory imported in AYON launcher
 # - do not specify if there is no client code
-client_dir = None
+client_dir = "ayon_substancedesigner"
 
 # Version compatibility with AYON server
-# ayon_server_version = ">=1.0.7"
+ayon_server_version = ">=1.1.2"
 # Version compatibility with AYON launcher
 # ayon_launcher_version = ">=1.0.2"
 
 # Mapping of addon name to version requirements
 # - addon with specified version range must exist to be able to use this addon
-ayon_required_addons = {}
+ayon_required_addons = {
+    "core": ">=1.0.12",
+}
 # Mapping of addon name to version requirements
 # - if addon is used in same bundle the version range must be valid
 ayon_compatible_addons = {}
