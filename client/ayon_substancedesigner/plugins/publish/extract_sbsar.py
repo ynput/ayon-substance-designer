47f61f5927f93b498d7e129442709804b8b54298
Kayla Man <moonyuet2008@hotmail.com>
2025-01-23 11:58:47 +0800

    make sure it exports the package where the users choose to publish

--- client/ayon_substancedesigner/plugins/publish/extract_sbsar.py
+++ client/ayon_substancedesigner/plugins/publish/extract_sbsar.py
@@ -3,6 +3,7 @@ import sd
 
 from ayon_core.pipeline import publish
 from sd.api.sbs.sdsbsarexporter import SDSBSARExporter
+from ayon_substancedesigner.api.lib import get_sd_graph_by_name
 
 
 class ExtractSbsar(publish.Extractor):
@@ -20,13 +21,18 @@ class ExtractSbsar(publish.Extractor):
         ctx = sd.getContext()
         exporterInstance = SDSBSARExporter(ctx, None)
         exporter = exporterInstance.sNew()
+
+        graph_name = instance.data["graph_name"]
+        sd_graph = get_sd_graph_by_name(graph_name)
+
         current_file = instance.context.data["currentFile"]
         filename = os.path.basename(current_file)
         filename = filename.replace("sbs", "sbsar")
         staging_dir = self.staging_dir(instance)
         filepath = os.path.normpath(
             os.path.join(staging_dir, filename))
-        exporter.exportSBSFileToSBSAR(current_file, filepath)
+        # export the graph with filepath
+        exporter.exportPackageToSBSAR(sd_graph, filepath)
 
         if "representations" not in instance.data:
             instance.data["representations"] = []
